ad52ac223ebc9c11a73077727b1c6c922f17030f
introspective616 <gui21050@gmail.com>
2026-07-10 20:51:40 -0300

    mod: melhora na funcionalidade do programa

--- a_para_n.c
+++ a_para_n.c
@@ -5,15 +5,21 @@ int main() {
     long long int a, n, soma;
     printf("\nColoque um valor inteiro: ");
     scanf("%lld", &n);
-    printf("\nA = ");
-    for(a = 1; a <= n; a += 1){
-        if(a < n)
-            printf("%lld + ", a);
-        else 
-            printf("%lld ", a);
-        soma += a;
-        if(a == n)
-            printf("= %lld", soma);
+    if(n > 1){
+        printf("\nA = ");
+        for(a = 1; a <= n; a += 1){
+            if(a < n)
+                printf("%lld + ", a);
+            else 
+                printf("%lld ", a);
+            soma += a;
+            if(a == n)
+                printf("= %lld", soma);
+        }
     }
-    
+    else if(n <= 0) {
+        printf("\nValor não pode ser zero ou inferior para o programa\n");
+    }
+    else 
+        printf("\nA = 1");
 }
